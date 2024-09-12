1eecff0a690a1952842afe5c18c0c203059e8e25
md5502 <m.baniasadi.d@gmail.com>
2024-09-12 17:42:07 +0330

    add the users url to the main  url file

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("notes.urls", namespace="notes")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
