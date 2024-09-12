4c79150af338c08c3699ad4370bc363848dca636
md5502 <m.baniasadi.d@gmail.com>
2024-09-12 10:43:50 +0330

    add some fields to the note model and remigrate it

--- notes/models.py
+++ notes/models.py
@@ -1,17 +1,23 @@
+import markdown
+from autoslug import AutoSlugField
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-
-
 class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     title = models.CharField(max_length=128)
     body = models.TextField()
+    rendered_html = models.TextField(editable=False, default="")
+    slug = AutoSlugField(populate_from="title", unique_with=["created_at"])
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.rendered_html = markdown(self.body)
+        super().save(*args, **kwargs)
