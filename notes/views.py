2fd15740ae4cd49b1aa9f5a0f844e827af700c27
md5502 <m.baniasadi.d@gmail.com>
2024-09-12 11:27:58 +0330

    adding the note urls to the main url

--- notes/views.py
+++ notes/views.py
@@ -1,2 +1,35 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import NoteCreateForm
+from .models import Note
 
 # Create your views here.
+
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, "note_list.html", {"notes": notes})
+
+@login_required(login_url = "/users/login")
+def note_create(request):
+    if request.method == "POST":
+        form = NoteCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.owner = request.user
+            note.save()
+            messages.success(request, "the note create successfully")
+            return redirect("/")
+    else:
+        form = NoteCreateForm()
+    return render(request, "note_create", {"form": form})
+
+
+@login_required(login_url = "/users/login")
+def render_html(request, slug):
+    note = get_object_or_404(Note, slug=slug)
+    if note.owner == request.user:
+        return render(request, "note_detail.html", {"note": note})
+    messages.error(request, "you have to be the owner of the note")
+    return redirect("note_list")
