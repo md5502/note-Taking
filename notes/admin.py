be68d6afcc636462c7a5525b7b64db0c27a86a2e
md5502 <m.baniasadi.d@gmail.com>
2024-09-12 10:07:27 +0330

    Create Note model and migrate it to database and register to the admin site

--- notes/admin.py
+++ notes/admin.py
@@ -1,2 +1,11 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Note
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display =["title", "owner", "created_at", "updated_at"]
+    list_filter=["created_at", "updated_at"]
+
+
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,2 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.title
